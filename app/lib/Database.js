499f5bf864d39212b13225669eb498b163d95b6c
AsherSSB <gromanenghi04@gmail.com>
2025-09-30 15:19:34 -0700

    adds testing db functions

--- app/lib/Database.js
+++ app/lib/Database.js
@@ -16,14 +16,14 @@ async function initializeTables() {
             password varchar(255) NOT NULL,
             name varchar(255) NOT NULL
         );
-        `, (err, res) => {
-            if (err) {
-                console.error('Error creating users table', err);
-                return false;
-            } else {
-                console.log('Successfully initialized users table', res);
-                return true;
-            }
+    `, (err, res) => {
+        if (err) {
+            console.error('Error creating users table', err);
+            return false;
+        } else {
+            console.log('Successfully initialized users table');
+            return true;
+        }
     });
 
     // a lot of this table is allowed to be null to account for deleted users
@@ -38,16 +38,15 @@ async function initializeTables() {
             body text,
             date_created timestamp
         );
-        `, (err, res) => {
-            if (err) {
-                console.error('Error creating blogs table', err);
-                return false;
-            } else {
-                console.log('Successfully initialized blogs table', res);
-                return true;
-            }
+    `, (err, res) => {
+        if (err) {
+            console.error('Error creating blogs table', err);
+            return false;
+        } else {
+            console.log('Successfully initialized blogs table');
+            return true;
         }
-    );
+    });
     
     if (userTableCreated && blogsTableCreated) {
         return true;
@@ -56,6 +55,39 @@ async function initializeTables() {
     return false;
 }
 
+async function getAllBlogs() {
+    const blogs = await pool.query(`
+        SELECT * 
+        FROM blogs; 
+    `, (err, res) => {
+        if (err) {
+            console.error('Error occured while getting blogs', err);
+            return [];
+        } else {
+            return res;
+        }
+    });
+
+    return blogs;
+}
+
+async function postBlog() {
+    const result = await pool.query(`
+        INSERT INTO blogs (creator_name, creator_user_id, title, category, body)
+        VALUES ($1, $2, $3, $4, $5);
+    `, (err, res) => {
+        if (err) {
+            console.error("Error while adding blog to database", err);
+            return false;
+        } else {
+            return true
+        }
+    });
+
+    return result;
+}
+
 module.exports = {
-    initializeTables
+    initializeTables,
+    getAllBlogs
 }
