3a23449327d4d1e8c91d8e14f0b2040725bbb007
AsherSSB <gromanenghi04@gmail.com>
2025-10-01 12:52:15 -0700

    adds signup route

--- app/routes/homeRoutes.js
+++ app/routes/homeRoutes.js
@@ -7,4 +7,9 @@ router.get('/', async (req, res) => {
     return res.render('home/index.ejs', {blogs: blogs} );
 });
 
+router.get('/signup', async (req, res) => {
+    console.log('SIGNUP INVOKED');
+    return res.render('home/signup.ejs', {} );
+});
+
 module.exports = router;
